926fe45b7c4f9ac06165e607f11cd190cfa73445
Chirag Makvana <mchirag441@gmail.com>
2025-09-09 11:54:23 +0530

    Auto-generated commit

--- server/assets-chunks/index_csr_html.mjs
+++ server/assets-chunks/index_csr_html.mjs
@@ -1,13 +1,14 @@
-<!doctype html>
+export default `<!doctype html>
 <html lang="en" data-beasties-container="">
 <head>
   <meta charset="utf-8">
   <title>FacebookMetaApiFront</title>
-  <base href="https://ChiragMakvana09.github.io/whtsappDemo_form/">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-KJHD4GU3.js" type="module"></script></body>
+<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-WRQGYMX4.js" type="module"></script></body>
 </html>
+`;
